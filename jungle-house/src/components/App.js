935d60bb566b06a804e8c1153d90cf7c5ebc5bd6
Phos23 <phos23@gmail.com>
2021-11-01 20:44:44 +0000

    add questionform

--- jungle-house/src/components/App.js
+++ jungle-house/src/components/App.js
@@ -1,12 +1,14 @@
 import Banner from './Banner';
 import Cart from './Cart';
 import ShoppingList from './ShoppingList';
+import QuestionForm from './QuestionForm';
 
 function App() {
   return (
     <div>
       <Banner />
       <Cart />
+      <QuestionForm />
       <ShoppingList />
     </div>
   )
---
+++ jungle-house/src/components/QuestionForm.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+function handleSubmit(e) {
+    e.preventDefault();
+    alert(e.target['my_input'].value)
+}
+
+function QuestionForm() {
+    const [inputValue, setInputValue] = useState('Ask your question here')
+    return (
+        // <form onSubmit={handleSubmit}>
+        //     <input type="text" name='my_input' defaultValue='Type your text' />
+        //     <button type='submit'>Enter</button>
+        // </form>
+        <div>
+            <textarea
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+            />
+            <button onClick={() => alert(inputValue)}>Alert Me!</button>
+        </div>
+    )
+}
+
+export default QuestionForm
