4c7f24aada1b916ef5970471cdb823f7243f6e0d
Phos23 <phos23@gmail.com>
2021-11-01 20:45:02 +0000

    recover cover image

--- jungle-house/src/components/PlantItem.js
+++ jungle-house/src/components/PlantItem.js
@@ -1,12 +1,18 @@
 import CareScale from './CareScale'
 import '../styles/PlantItem.css'
 
+function handleClick(e) {
+    console.log('This is my event:', e);
+}
+
 
 function PlantItem({ name, cover, id, light, water }) {
+
+
     return (
 
-        <li key={id} className='jh-plant-item'>
-            {/* <img src={cover} alt={`${name} cover`} className="jh-plant-item-cover" /> */}
+        <li key={id} className='jh-plant-item' onClick={handleClick}>
+            <img src={cover} alt={`${name} cover`} className="jh-plant-item-cover" />
 
             {name}
 
