2df295ed444bcaa8cd5b2b90217a4cc908f2e104
Phos23 <phos23@gmail.com>
2021-11-03 18:40:49 +0000

    sync latest update

--- jungle-house/src/components/App.js
+++ jungle-house/src/components/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Banner from './Banner'
 import logo from '../assets/logo.png'
 import Cart from './Cart'
@@ -7,20 +7,25 @@ import ShoppingList from './ShoppingList'
 import '../styles/Layout.css'
 
 function App() {
-	const [cart, updateCart] = useState([])
-	return (
-		<div>
-			<Banner>
-				<img src={logo} alt='Jungle House' className='jh-logo' />
-				<h1 className='jh-title'>Jungle House</h1>
-			</Banner>
-			<div className='jh-layout-inner'>
-				<Cart cart={cart} updateCart={updateCart} />
-				<ShoppingList cart={cart} updateCart={updateCart} />
-			</div>
-			<Footer />
-		</div>
-	)
+  const savedCart = localStorage.getItem('cart')
+  const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart])
+
+  return (
+    <div>
+      <Banner>
+        <img src={logo} alt='Jungle House' className='jh-logo' />
+        <h1 className='jh-title'>Jungle House</h1>
+      </Banner>
+      <div className='jh-layout-inner'>
+        <Cart cart={cart} updateCart={updateCart} />
+        <ShoppingList cart={cart} updateCart={updateCart} />
+      </div>
+      <Footer />
+    </div>
+  )
 }
 
 export default App
--- jungle-house/src/components/Footer.js
+++ jungle-house/src/components/Footer.js
@@ -2,32 +2,32 @@ import { useState } from 'react'
 import '../styles/Footer.css'
 
 function Footer() {
-	const [inputValue, setInputValue] = useState('')
+  const [inputValue, setInputValue] = useState('')
 
-	function handleInput(e) {
-		setInputValue(e.target.value)
-	}
+  function handleInput(e) {
+    setInputValue(e.target.value)
+  }
 
-	function handleBlur() {
-		if (!inputValue.includes('@')) {
-			alert("Attention, there is no @, this is not a valid address 😥")
-		}
-	}
+  function handleBlur() {
+    if (!inputValue.includes('@')) {
+      alert(
+        'Error: This is not a valid address because it is missing an @ symbol 😥'
+      )
+    }
+  }
 
-	return (
-		<footer className='jh-footer'>
-			<div className='jh-footer-elem'>
-			For plant enthusiasts 🌿🌱🌵
-			</div>
-			<div className='jh-footer-elem'>Subscribe to our newsletter :</div>
-			<input
-				placeholder='Enter your email'
-				onChange={handleInput}
-				value={inputValue}
-				onBlur={handleBlur}
-			/>
-		</footer>
-	)
+  return (
+    <footer className='jh-footer'>
+      <div className='jh-footer-elem'>For plant enthusiasts 🌿🌱🌵</div>
+      <div className='jh-footer-elem'>Subscribe to our newsletter:</div>
+      <input
+        placeholder='Enter your email'
+        onChange={handleInput}
+        value={inputValue}
+        onBlur={handleBlur}
+      />
+    </footer>
+  )
 }
 
 export default Footer
